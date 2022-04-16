721fc12a48bc361e82c79d750fff3733c1c6ec52
ulldecorb <ausias.bertran@gmail.com>
2022-04-16 18:22:37 +0200

    Add iife generator of span

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-//     TODO
-// 
-//   1# clonar node ] add class "snake"
-//   2# fade-in : insert child nodes
-//   3# fade-out 
-// 
-// 
-// 
+/*    
+    TODO:
 
+    1# create dom element && add class "snake"
+    2# fade-in : insert child nodes on container
+    3# fade-out : fade out loading element
 
+*/
 
-    const container = document.getElementById('container');
-    const loader = document.getElementById('load');
-    const snakes = document.getElementsByClassName("snake");
+const container = document.getElementById('container');
+const fadeInDelay = [ 100 , 1000, 1500, 2000, 2500, 3000 ];
+const fadeOutDelay = [ 1500 , 1250, 1000, 750, 500, 250 ];
+const snakeSize = [ '30vmin', '37vmin', '44vmin', '51vmin', '58vmin', '65vmin' ];
 
-    document.body.addEventListener("click", fadeOut);
+// change listener to window.onload 
+document.body.addEventListener("click", fadeOutSnakes);
 
-const colorBox = [ "red", "blue", "cyan", "pink", "green", "yellow"]
-const animationTimer = ["0.5s;", "1s", "1.5s", "2s", "2.5s", "3s"]
-const snakeSize = [ "63vmin", "56.7vmin", "51.3vmin", "45.927vmin", "41.334vmin", "37.2vmin" ]
+(function createSnakes () {
+    for ( i = 0; i < 6; i ++) {
+        const spanSize = snakeSize[i]
+        const delay = fadeInDelay[i];
+        const span = document.createElement("span");
+        span.style.width = spanSize;
+        span.style.height = spanSize;
+        span.classList.add('snake');
 
-
-function fadeOut () {
-    for ( i = 0; i < snakes.length; i ++) {
-        const snake = document.getElementsByClassName("snake")[i];
-        const timeMultipler = 250 * (1 + i);
         setTimeout(() => {
-            snake.style.height = "125vmax";
-            snake.style.width = "125vmax";
-            snake.style.border = '8px solid rgb(110, 92, 12)'
-        }, timeMultipler);
+            container.appendChild(span);
+        }, delay);
     }
+})()
+
+function fadeOutSnakes () {
+    const snakes = document.getElementsByClassName("snake");
+    for (i = 5; i >= 0; i --) {
+        const snake = snakes[i];
+        const delay = fadeOutDelay[i];
+
+        setTimeout(() => {
+            snake.style.height = "150vmax";
+            snake.style.width = "150vmax";
+            snake.style.border = '4px solid rgb(110, 92, 12)'
+        }, delay);
+    }
+
     setTimeout(() => {
         container.style.opacity = '0';
     }, 2100);
 }
-document.body.addEventListener("click", fadeOut);
