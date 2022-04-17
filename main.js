999f0f8da0f5cf5a10b91722bf75fc423cf4eeea
ulldecorb <ausias.bertran@gmail.com>
2022-04-17 02:32:41 +0200

    Create mixins sass file Create variables sass file

--- main.js
+++ main.js
@@ -3,20 +3,20 @@
 
     1# create dom element && add size and class "snake"
     2# fade-in : insert child nodes on container by delay
-    3# fade-out : fade out loading elements by delay
+    3# fade-out : fade out transition elements by delay
 
 */
 
 const container = document.getElementById('container');
-const fadeInDelay = [ 100 , 1000, 1500, 2000, 2500, 3000 ];
-const fadeOutDelay = [ 1500 , 1250, 1000, 750, 500, 250 ];
-const snakeSize = [ '30vmin', '37vmin', '44vmin', '51vmin', '58vmin', '65vmin' ];
+const fadeInDelay = [ 700, 1400, 2100, 2800, 3500, 4200, 4900, 5600 ];
+const fadeOutDelay = [ 1750, 1500 , 1250, 1000, 750, 500, 250, 0 ];
+const snakeSize = [ '30.6vmin', '36.25vmin', '41.88vmin', '47.5vmin', '53.13vmin', '58.75vmin', '64.36vmin', '70vmin' ];
 
 // change listener to window.onload 
 document.body.addEventListener("click", fadeOutSnakes);
 
 (function createSnakes () {
-    for ( i = 0; i < 6; i ++) {
+    for ( i = 0; i < 8; i ++) {
         const spanSize = snakeSize[i]
         const delay = fadeInDelay[i];
         const span = document.createElement("span");
@@ -32,7 +32,7 @@ document.body.addEventListener("click", fadeOutSnakes);
 
 function fadeOutSnakes () {
     const snakes = document.getElementsByClassName("snake");
-    for (i = 5; i >= 0; i --) {
+    for (i = 7; i >= 0; i --) {
         const snake = snakes[i];
         const delay = fadeOutDelay[i];
 
